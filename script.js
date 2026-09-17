bee5ac6be5ba7603379b1ea8bd0f1da3ec9ef14b
icasszy <154059740+icasszy@users.noreply.github.com>
2026-09-17 14:45:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const menuToggle = document.getElementById('menuToggle');
+const navMenu = document.getElementById('navMenu');
+
+menuToggle.addEventListener('click', () => {
+  navMenu.classList.toggle('show');
+});
+
+document.querySelectorAll('.nav-menu a').forEach(link => {
+  link.addEventListener('click', () => navMenu.classList.remove('show'));
+});
